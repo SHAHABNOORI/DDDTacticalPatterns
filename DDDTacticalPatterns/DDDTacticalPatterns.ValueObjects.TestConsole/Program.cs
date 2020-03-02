7e312638fd864a4322b96fd87c3ef259d9820dde
shahabnoori <noorigoodarzi@gmail.com>
2020-03-02 19:28:44 +0330

    Initial DDDTacticalPatterns.ValueObjects.TestConsole Project

---
+++ DDDTacticalPatterns/DDDTacticalPatterns.ValueObjects.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDDTacticalPatterns.ValueObjects.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DDDTacticalPatterns/DDDTacticalPatterns.ValueObjects/MoneyV1/Money.cs
@@ -0,0 +1,17 @@
+﻿namespace DDDTacticalPatterns.ValueObjects.MoneyV1
+{
+    public class Money
+    {
+        protected readonly decimal Value;
+
+        public Money() : this(0)
+        {
+
+        }
+
+        public Money(decimal value)
+        {
+            Value = value;
+        }
+    }
+}
